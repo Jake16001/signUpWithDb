c56e0d00d228274c2a2747b4c0c112f5e292e267
Jake16001 <160814225+Jake16001@users.noreply.github.com>
2024-02-28 09:06:03 -0600

    Create 1.1.py

---
+++ v1/1.1.py
@@ -0,0 +1,32 @@
+import getpass
+import re
+
+def is_valid_email(email):
+      pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+      return re.match(pattern, email)
+
+def signup():
+      while True:
+          username = input("Enter a username: ")
+          password = getpass.getpass("Enter a password: ")
+          confirm_password = getpass.getpass("Confirm password: ")
+          email = input("Enter your email: ")
+          if is_valid_email(email):
+              if password == confirm_password:
+                  print("Signup successful!")
+                  break
+              else:
+                  print("Your passwords do not match. Please try again.")
+          else:
+              print("Invalid email format. Please enter a valid email address.")
+
+def login():
+      login_username = input("Enter your username: ")
+      login_password = getpass.getpass("Enter your password: ")
+      print("Login successful!")
+
+answer = input("Do you have an account? (yes/no): ")
+if answer.upper() == "YES":
+      login()
+elif answer.upper() == "NO":
+      signup()
